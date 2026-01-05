63dbd58dd27a44d46eacdda78c9f5f4556631dea
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-05 16:42:53 +0100

    Mark string as non-translatable + externalize variables

--- common/plugins/org.polarsys.capella.common.flexibility.wizards/src/org/polarsys/capella/common/flexibility/wizards/renderer/Messages.java
+++ common/plugins/org.polarsys.capella.common.flexibility.wizards/src/org/polarsys/capella/common/flexibility/wizards/renderer/Messages.java
@@ -4,6 +4,9 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
+	public static String BrowseRenderer_1;
+	public static String BrowseRenderer_3;
+	public static String BrowseRenderer_4;
 	public static String CopyTextRenderer_0;
 	static {
 		// initialize resource bundle
