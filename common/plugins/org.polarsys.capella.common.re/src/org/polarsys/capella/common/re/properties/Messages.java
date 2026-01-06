a7a2a5c9e1c460c58cfe392f2d94fff340ffcd53
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-06 10:38:43 +0100

    Externalized UI strings

--- common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/properties/Messages.java
+++ common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/properties/Messages.java
@@ -5,6 +5,7 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
 	public static String LocationTargetProperty_0;
+	public static String SharedElementsProperty_0;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
