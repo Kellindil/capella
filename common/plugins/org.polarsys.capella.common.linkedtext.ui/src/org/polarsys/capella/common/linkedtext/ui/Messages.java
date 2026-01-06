47ee03e983c8cfa01d1d51ac8e60402d511170bc
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-06 09:45:14 +0100

    Externalized UI messages + marked technical string as non translatable

--- common/plugins/org.polarsys.capella.common.linkedtext.ui/src/org/polarsys/capella/common/linkedtext/ui/Messages.java
+++ common/plugins/org.polarsys.capella.common.linkedtext.ui/src/org/polarsys/capella/common/linkedtext/ui/Messages.java
@@ -1,12 +1,10 @@
-package org.polarsys.capella.common.libraries.provider;
+package org.polarsys.capella.common.linkedtext.ui;
 
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
-	public static String LibraryReferenceItemProvider_0;
-	public static String LibraryReferenceItemProvider_5;
-	public static String ModelInformationItemProvider_0;
+	public static String EmbeddedLinkedTextEditor_0;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
